bf55b9e45e7e268f935ebadb5d224dead7b15f34
Caetano-Ventura-Coutinho <currytano@gmail.com>
2024-03-15 10:13:58 -0300

    Removendo comentários de todas as instâncias de console.log

--- lista2.js
+++ lista2.js
@@ -1,5 +1,5 @@
 const tioBen = "Grandes poderes trazem grandes responsabilidades"
-//console.log(tioBen)
+console.log(tioBen)
 /*
 
 */
@@ -7,67 +7,67 @@ const tom = 'Tom'
 const firstTom = 'Hanks'
 const secondTom = 'Cruise'
 const myFavoriteTom = tom + ' ' + firstTom
-//console.log(myFavoriteTom)
+console.log(myFavoriteTom)
 /*
 
 */
 const programmingLanguage = 'JavaScript'
 const js = programmingLanguage[0] + programmingLanguage[4]
-//console.log(js)
+console.log(js)
 /*
 
 */
 const mozilla = "Mozilla Developer Network"
 const mdn = mozilla[0] + mozilla[8] + mozilla[18]
-//console.log(mdn)
+console.log(mdn)
 /*
 
 */
-//console.log(programmingLanguage.length)
+console.log(programmingLanguage.length)
 /*
 
 */
 let randomWord = "Araponga"
 randomWord = "Maçã com til"
-//console.log(randomWord[randomWord.length - 1])
+console.log(randomWord[randomWord.length - 1])
 /*
 
 */
 const summerMessage = 'O VERÃO CHEGA NESTE FIM DE SEMANA'
-//console.log(summerMessage.toLowerCase())
+console.log(summerMessage.toLowerCase())
 /*
 
 */
 const winterMessage = "o inverno está chegando"
-//console.log(winterMessage.toUpperCase())
+console.log(winterMessage.toUpperCase())
 /*
 
 */
-//console.log(winterMessage.indexOf('v'))
+console.log(winterMessage.indexOf('v'))
 /*
 
 */
 const email = 'laracroft@gmail.com.br'
-//console.log(email.lastIndexOf('a'))
+console.log(email.lastIndexOf('a'))
 /*
 
 */
-//console.log(email.slice(0,9))
-//console.log(email.slice(10,15))
-//console.log(email.replace("laracroft", "chun-li"))
+console.log(email.slice(0,9))
+console.log(email.slice(10,15))
+console.log(email.replace("laracroft", "chun-li"))
 /*
 
 */
 let num1 = 10
 let num2 = 3
 // +, -, *, /,
-//console.log(num1 ** num2)
+console.log(num1 ** num2)
 /*
 
 */
 
 const crazyOperation = 5 * (10-3) ** 2
-//console.log(crazyOperation)
+console.log(crazyOperation)
 /*
 
 */
