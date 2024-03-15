ba9e2ce576f39c607672c8ce4b7750495b88ae78
Caetano-Ventura-Coutinho <currytano@gmail.com>
2024-03-15 10:02:57 -0300

    Criando pasta CJAM_A e arquivo lista2.js

---
+++ lista2.js
@@ -0,0 +1,76 @@
+const tioBen = "Grandes poderes trazem grandes responsabilidades"
+//console.log(tioBen)
+/*
+
+*/
+const tom = 'Tom'
+const firstTom = 'Hanks'
+const secondTom = 'Cruise'
+const myFavoriteTom = tom + ' ' + firstTom
+//console.log(myFavoriteTom)
+/*
+
+*/
+const programmingLanguage = 'JavaScript'
+const js = programmingLanguage[0] + programmingLanguage[4]
+//console.log(js)
+/*
+
+*/
+const mozilla = "Mozilla Developer Network"
+const mdn = mozilla[0] + mozilla[8] + mozilla[18]
+//console.log(mdn)
+/*
+
+*/
+//console.log(programmingLanguage.length)
+/*
+
+*/
+let randomWord = "Araponga"
+randomWord = "Maçã com til"
+//console.log(randomWord[randomWord.length - 1])
+/*
+
+*/
+const summerMessage = 'O VERÃO CHEGA NESTE FIM DE SEMANA'
+//console.log(summerMessage.toLowerCase())
+/*
+
+*/
+const winterMessage = "o inverno está chegando"
+//console.log(winterMessage.toUpperCase())
+/*
+
+*/
+//console.log(winterMessage.indexOf('v'))
+/*
+
+*/
+const email = 'laracroft@gmail.com.br'
+//console.log(email.lastIndexOf('a'))
+/*
+
+*/
+//console.log(email.slice(0,9))
+//console.log(email.slice(10,15))
+//console.log(email.replace("laracroft", "chun-li"))
+/*
+
+*/
+let num1 = 10
+let num2 = 3
+// +, -, *, /,
+//console.log(num1 ** num2)
+/*
+
+*/
+
+const crazyOperation = 5 * (10-3) ** 2
+//console.log(crazyOperation)
+/*
+
+*/
+let postLikes = 10
+postLikes *= 10
+console.log(postLikes)
